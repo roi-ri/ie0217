28775707eefa2c5328db1409ae2420fbbc8a96da
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-08-17 13:04:15 -0600

    se agrega cambio en el codigo

--- Sesiones/Sesion1/holaMundo.cpp
+++ Sesiones/Sesion1/holaMundo.cpp
@@ -1,6 +1,8 @@
 #include <iostream> 
 int main(){
-    std::cout << "Hola mundo, estudiantes de IE0217";
+    std::cout << "Hola a todos y todas, estudiantes de IE0217\n";
+    std::cout << "Probando el stash!";  // Añadimos una nueva línea de código al final de la línea anterior
     return 0; 
 }
 
+ 
---
+++ Sesiones/Sesion1/otroArchivo.cpp
@@ -0,0 +1,5 @@
+#include <iostream> 
+int main(){
+    std::cout << "Hola mundo, desde otro archivo\n";
+    return 0; 
+}
