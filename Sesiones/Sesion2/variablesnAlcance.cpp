eb302f02f280aff9c2dde5a927b0cc9e4344751a
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-08-20 01:07:59 -0600

    Se agrega continuacion en variablesnAlcances.cpp referente a la sobre carga de funciones con usos incorrectos y correctos de estas mismas
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

---
+++ Sesiones/Sesion2/variablesnAlcance.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+
+using namespace std; 
+
+
+void test(){
+
+    //var is a static variable
+    static int var = 0; 
+
+    ++var;
+
+    cout << var << endl; 
+}
+
+
+int main(){
+
+    //le asigna el valor por defecto que va adoptando el elemento var 
+
+    test();
+    test();
+
+    return 0;
+}
+
+
+// ejemplo 2
+
+int cuenta; 
+extern void write_extern();
+
+main(){
+    cuenta = 5; 
+    write_extern();
+}
+
+
+extern int count; 
+
+void write_extern(void){
+    std::cout << "count is " << cuenta << std::endl;
+}
+
