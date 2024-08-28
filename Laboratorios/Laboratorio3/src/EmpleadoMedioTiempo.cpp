aff71322bcc02f3fdc4587bc96c5540739ed9921
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-08-27 19:33:09 -0600

    Se agrega documentacion a los archivos
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Laboratorios/Laboratorio3/src/EmpleadoMedioTiempo.cpp
+++ Laboratorios/Laboratorio3/src/EmpleadoMedioTiempo.cpp
@@ -3,21 +3,39 @@
 
 using namespace std;
 
-
+/*
+Constructor de la clase EmpleadoMedioTiempo
+Inicializa el nombre, ID, tarifa por hora y horas trabajadas del empleado de medio tiempo.
+Parámetros:
+nombre: El nombre del empleado.
+tarifaPorHora: La tarifa por hora del empleado de medio tiempo.
+horasTrabajadas: El número de horas trabajadas por el empleado.
+id: El identificador del empleado.
+*/
 EmpleadoMedioTiempo::EmpleadoMedioTiempo(const string &nombre, int id, double tarifaPorHora, int horasTrabajadas) : Empleado(nombre, id, tarifaPorHora * horasTrabajadas), tarifaPorHora(tarifaPorHora), horasTrabajadas(horasTrabajadas){}
 
+// Establece el número de horas trabajadas por el empleado.
 void EmpleadoMedioTiempo::setHorasTrabajadas(int horas){
     this->horasTrabajadas = horas;
 }
 
+// Establece la tarifa por hora del empleado.
 void EmpleadoMedioTiempo::setTarifaPorHora(double tarifa){
     this->tarifaPorHora = tarifa;
 }
-
+// Devuelve el salario del empleado de medio tiempo, calculado como la tarifa por hora multiplicada por las horas trabajadas.
 double EmpleadoMedioTiempo::getSalario() const{
     return tarifaPorHora * horasTrabajadas;
 
 }
+
+/*
+Muestra la información del empleado de medio tiempo, incluyendo el nombre, ID, salario, horas trabajadas y tarifa por hora.
+Utiliza la sobrecarga del operador << para mostrar la información básica del empleado.
+Retorno:
+La información del empleado se imprime en la salida estándar.
+*/
+
 
 void EmpleadoMedioTiempo::mostrarInformacion() const{
     cout << *this << ", Horas Trabajadas: " << horasTrabajadas << "Tarifa por Hora: " << tarifaPorHora << endl; 
--- Laboratorios/Laboratorio3/src/main.cpp
+++ Laboratorios/Laboratorio3/src/main.cpp
@@ -8,23 +8,26 @@
 using namespace std;
 
 int main() {
-    vector<Empleado*> empleados;
 
+    // Vector que almacena punteros a objetos de tipo Empleado
+    vector<Empleado*> empleados;
+    // Agrega empleados de diferentes tipos al vector
     empleados.push_back(new EmpleadoTiempoCompleto("Alice", 1, 50000, 5000, 1000));
     empleados.push_back(new EmpleadoMedioTiempo("Bob", 2, 20, 120));
     empleados.push_back(new EmpleadoTiempoCompleto("Charlie", 3, 55000, 4000, 1500));
     empleados.push_back(new EmpleadoMedioTiempo("David", 4, 25, 100));
     empleados.push_back(new EmpleadoTiempoCompleto("Esteban", 10, 150000, 9000, 60000));
 
+    // Recorre el vector e imprime la información de cada empleado
     for (const auto& empleado: empleados){
         empleado->mostrarInformacion();
 
     }
 
-
+     // Libera la memoria asignada a cada objeto empleado
     for (const auto& empleado : empleados){
 
         delete empleado;
     }
-
+    return 0; //Fin del programa
 }
