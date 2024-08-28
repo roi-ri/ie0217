e19f5fb60b1707ea1db0d91d37f0f809d6031e07
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-08-27 19:23:26 -0600

    Se agrega documentaciona al documento Empleado.cpp
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Laboratorios/Laboratorio3/src/Empleado.cpp
+++ Laboratorios/Laboratorio3/src/Empleado.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 
 Empleado::Empleado(const string &nombre, int id, double salario) : nombre(nombre), id(id), salario(salario){}; // Igual que hacer el this->nombre, this->id, etc...
+// Constructor de la clase empleado
+// INicializa el nombre, ID y salario del empleado
 
 void Empleado::setNombre(const string &nombre) {
+    // Establece el nombre del empleado
     this->nombre = nombre;
 };
 
 string Empleado::getNombre() const {
+    
     return nombre;
     // Devuelve una copia de nombre (evita la modificación del nombre original)
 };
@@ -29,14 +33,26 @@ void Empleado::setSalario(double salario){
 double Empleado::getSalario() const {
     return salario;
 }
-
-
+/*
+Compara el salario de dos empleados.
+arámetros:
+e1: Primer empleado a comparar.
+e2: Segundo empleado a comparar.
+Retorno: true si el salario del primer empleado es mayor que el del segundo, false en caso contrario.
+*/
 bool compararSalario(const Empleado &e1, const Empleado &e2){
    return e1.salario > e2.salario; 
 }
 
-
-
+/*
+ Sobrecarga del operador de inserción << para la clase Empleado.
+ Permite mostrar la información del empleado en un flujo de salida (como cout).
+ Parámetros:
+ os: El flujo de salida donde se insertará la información del empleado.
+ empleado: El empleado cuya información se mostrará.
+ Retorno:
+ El flujo de salida actualizado.
+*/
 ostream &operator<<(ostream &os, const Empleado &empleado){
         os << "Nombre: " << empleado.nombre << ", ID: " << empleado.id << ", Salario: " << empleado.salario;
         return os;
