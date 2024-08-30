8925e5dc55d5190772a94febec9affec4cec5886
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-08-30 11:04:11 -0600

    Se agregan licencias a los archivos
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Tareas/Tarea1/src/calcEstadyBusq.hpp
+++ Tareas/Tarea1/src/calcEstadyBusq.hpp
@@ -1,3 +1,20 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2024 [Rodrigo Sánchez, UNiversidad de Costa Rica]
+ * 
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include<iostream>
 #include <cstdlib> // Se utiliza para poder utilizar la funcion atoi y convertir los string a numero
 
