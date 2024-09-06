e7ebf3b6e6e3216a16f8d04370884c70dbc906eb
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-06 11:24:34 -0600

    Se agrega Clase base cliente y su respectiva implementacion
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

---
+++ Tareas/Tarea2/src/Cliente.cpp
@@ -0,0 +1,18 @@
+#include"Cliente.hpp" 
+
+Cliente::Cliente(const string& nombre) : nombre(nombre) {};
+
+
+void Cliente::agregarProducto(Producto* producto){
+    carrito.push_back(producto);
+}
+
+
+double Cliente::calcularTotal() const {
+    double total = 0.0;
+    for (const auto& producto : carrito) {
+        total += producto->getPrecio();
+    }
+    return total;
+}
+
