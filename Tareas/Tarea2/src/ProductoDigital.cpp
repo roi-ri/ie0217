f056a00866d5885fd953466dc24909aad5ea1250
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-06 13:55:51 -0600

    Se agrega documentacino tipo doxygen a los archivos de la clase dedrivada de producto digital
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Tareas/Tarea2/src/ProductoDigital.cpp
+++ Tareas/Tarea2/src/ProductoDigital.cpp
@@ -1,8 +1,24 @@
 #include"ProductoDigital.hpp"
 
-
+/**
+ * @brief Constructor de la clase `ProductoDigital`.
+ * @param nombre Nombre del producto.
+ * @param precio Precio del producto.
+ * @param id Identificador único del producto digital.
+ *
+ * Este constructor inicializa un objeto `ProductoDigital` con el nombre, precio e identificador proporcionados.
+ * Llama al constructor de la clase base `Producto` para inicializar el nombre y el precio, 
+ * y luego inicializa el identificador del producto digital.
+ */
 ProductoDigital::ProductoDigital(const string &nombre, double precio, const string &id) : Producto(nombre, precio) , id(id){};
 
+
+/**
+ * @brief Obtiene el identificador del producto digital.
+ * @return El identificador del producto digital.
+ *
+ * Este método devuelve el identificador del producto digital almacenado en el objeto.
+ */
 string ProductoDigital::getId() const{
     return id; 
 }
--- Tareas/Tarea2/src/ProductoDigital.hpp
+++ Tareas/Tarea2/src/ProductoDigital.hpp
@@ -2,13 +2,35 @@
 #define PRODUCTODIGITAL_HPP
 
 #include"Producto.hpp"
-
+/**
+ * @class ProductoDigital
+ * @brief Representa un producto digital en la tienda.
+ *
+ * La clase `ProductoDigital` hereda de `Producto` y añade un atributo adicional para la identificación
+ * del producto digital. Representa productos que no tienen una forma física, como software o contenido
+ * descargable.
+ */
 class ProductoDigital : public Producto {
     private: 
+        /// Identificador único del producto digital.
         string id; 
 
     public: 
+        /**
+         * @brief Constructor de la clase `ProductoDigital`.
+         * @param nombre Nombre del producto.
+         * @param precio Precio del producto.
+         * @param id Identificador único del producto digital.
+         *
+         * Este constructor inicializa un objeto `ProductoDigital` con el nombre, precio e identificador proporcionados.
+         */
         ProductoDigital(const string &nombre, double precio, const string &id);
+        /**
+         * @brief Obtiene el identificador del producto digital.
+         * @return El identificador del producto digital.
+         *
+         * Este método devuelve el identificador del producto digital almacenado en el objeto.
+         */
         string getId() const; 
 };
 
