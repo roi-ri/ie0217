7de1b2484b3cef0d9379bce461c7abf8028beb15
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-06 13:17:27 -0600

    Se agrega documentacion con formato doxygen para archivo Tienda.hpp
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Tareas/Tarea2/src/Tienda.hpp
+++ Tareas/Tarea2/src/Tienda.hpp
@@ -3,16 +3,43 @@
 #include "Producto.hpp"
 #include "Cliente.hpp"
 
-
-class Tienda {
-    private: 
-        vector<Producto*> productosDisponibles; 
-        vector<Cliente*> clientes; 
-
+/**
+ * @class Tienda
+ * @brief Representa una tienda que gestiona productos y clientes.
+ *
+ * La clase `Tienda` permite agregar productos y clientes, y procesar compras realizadas por los clientes.
+ */
+class Tienda { 
+     
     public: 
+        /**
+         * @brief Agrega un producto a la tienda.
+         * @param producto Puntero al objeto `Producto` que se desea agregar.
+        *
+        * Este método almacena el puntero al objeto `Producto` en el vector de productos disponibles de la tienda.
+        */
         void agregarProducto(Producto* producto); 
-        void agregarCliente(Cliente* cliente); 
+        /**
+         * @brief Agrega un cliente a la tienda.
+        * @param cliente Puntero al objeto `Cliente` que se desea agregar.
+         *
+         * Este método almacena el puntero al objeto `Cliente` en el vector de clientes de la tienda.
+        */
+        void agregarCliente(Cliente* cliente);  
+         /**
+        * @brief Procesa la compra realizada por un cliente.
+        * @param nombreCliente Nombre del cliente que realiza la compra.
+        *
+         * Este método busca al cliente por su nombre y procesa la compra utilizando los productos disponibles en la tienda.
+        */
         void procesarCompra(const string &nombreCLiente);
+
+        /// Vector que contiene los productos disponibles en la tienda.    
+        vector<Producto*> productosDisponibles; 
+        
+
+        /// Vector que contiene los clientes de la tienda.
+        vector<Cliente*> clientes; 
 }; 
 
 
