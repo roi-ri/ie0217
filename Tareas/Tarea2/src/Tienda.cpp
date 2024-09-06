8fb258b07e44e54aa2a95c013181344be415a7e0
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-06 13:57:15 -0600

    Se agrega documentacion tipo doxygen a los archivos de la clase tienda.
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Tareas/Tarea2/src/Tienda.cpp
+++ Tareas/Tarea2/src/Tienda.cpp
@@ -1,5 +1,39 @@
 #include"Tienda.hpp" 
 
+
 void Tienda::agregarProducto(Producto* producto){
+    /**
+     * @brief Agrega un producto a la tienda.
+     * @param producto Puntero al objeto `Producto` que se desea agregar.
+     *
+     * Este método añade el puntero al objeto `Producto` al vector `productosDisponibles`.
+     */
     productosDisponibles.push_back(producto);
+}
+
+void Tienda::agregarCliente(Cliente* cliente){
+     /**
+     * @brief Agrega un cliente a la tienda.
+     * @param cliente Puntero al objeto `Cliente` que se desea agregar.
+     *
+     * Este método añade el puntero al objeto `Cliente` al vector `clientes`.
+     */
+    clientes.push_back(cliente);
+}
+
+void Tienda::procesarCompra(const string &nombreCliente){
+    /**
+     * @brief Procesa la compra realizada por un cliente.
+     * @param nombreCliente Nombre del cliente que realiza la compra.
+     *
+     * Este método busca al cliente en el vector `clientes` utilizando el nombre proporcionado.
+     * Si el cliente es encontrado, calcula el total a pagar aplicando un descuento y lo muestra en la consola.
+     */
+    for (const auto& cliente : clientes) {
+        if (cliente -> getNombre() == nombreCliente){
+            double total = cliente->aplicarDescuento();
+            cout<<"Total a pagar: " <<  total << endl;
+            break; 
+        }
+    }
 }
