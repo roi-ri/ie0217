ea524ce54b0276cbbd9c301eb39227ac31938971
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-06 13:56:37 -0600

    Se agrega documentacion tipo doxygen a los archivos de la clase derivada de ProductoFisico
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

--- Tareas/Tarea2/src/ProductoFisico.cpp
+++ Tareas/Tarea2/src/ProductoFisico.cpp
@@ -1,6 +1,23 @@
 #include"ProductoFisico.hpp"
 
+/**
+ * @brief Constructor de la clase `ProductoFisico`.
+ * @param nombre Nombre del producto.
+ * @param precio Precio del producto.
+ * @param peso Peso del producto en kilogramos.
+ *
+ * Este constructor inicializa un objeto `ProductoFisico` con el nombre, precio y peso proporcionados.
+ * Llama al constructor de la clase base `Producto` para inicializar el nombre y precio, 
+ * y luego inicializa el peso del producto físico.
+ */
 ProductoFisico::ProductoFisico(const string &nombre, double precio, double peso) : Producto(nombre, precio) , peso(peso){};
+
+/**
+ * @brief Obtiene el peso del producto físico.
+ * @return El peso del producto en kilogramos.
+ *
+ * Este método devuelve el peso del producto almacenado en el objeto.
+ */
 
 double ProductoFisico::getPeso() const{
     return peso;
--- Tareas/Tarea2/src/ProductoFisico.hpp
+++ Tareas/Tarea2/src/ProductoFisico.hpp
@@ -3,12 +3,35 @@
 
 #include"Producto.hpp"
 
+
+/**
+ * @class ProductoFisico
+ * @brief Representa un producto físico en la tienda.
+ *
+ * La clase `ProductoFisico` hereda de `Producto` e incluye una propiedad adicional para el peso.
+ * Representa productos que tienen una dimensión física y pueden requerir un manejo especial basado en su peso.
+ */
 class ProductoFisico : public Producto{
     private: 
+        /// Peso del producto en kilogramos.
         double peso; 
 
     public: 
+        /**
+         * @brief Constructor de la clase `ProductoFisico`.
+         * @param nombre Nombre del producto.
+         * @param precio Precio del producto.
+         * @param peso Peso del producto en kilogramos.
+         *
+         * Este constructor inicializa un objeto `ProductoFisico` con el nombre, precio y peso proporcionados.
+         */
         ProductoFisico(const string& nombre, double precio, double peso); 
+        /**
+         * @brief Obtiene el peso del producto físico.
+         * @return El peso del producto en kilogramos.
+         *
+         * Este método devuelve el peso del producto almacenado en el objeto.
+         */
         double getPeso() const; 
 
 };
