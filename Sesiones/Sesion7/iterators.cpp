e384fb045499483cb289b8797077c581e116ccef
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-13 14:39:31 -0600

    Se agrega implementacion de iteradores en c++
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

---
+++ Sesiones/Sesion7/iterators.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <forward_list>
+using namespace std;
+
+int main() {
+    // Crea un vector como string
+    vector<string> languages = {"Python", "C++", "Java"};
+    
+    // Crea un iterador para el vecetor string
+    vector<string>::iterator itr;
+
+    // Itera por todos los elementos del vector string
+    for (itr = languages.begin(); itr != languages.end(); itr++) {
+        cout << *itr << " ";  // print cada elemento del vector
+    }
+
+
+    // Crear una forward_list de enteros
+    forward_list<int> nums{1, 2, 3, 4};
+
+    // Inicializar un iterador al inicio de la forward_list
+    forward_list<int>::iterator itr = nums.begin();
+
+    // Iterar a través de la lista
+    while (itr != nums.end()) {
+        // Acceder al valor en la posición del iterador y guardarlo en original_value
+        int original_value = *itr;
+
+        // Duplicar el valor en la posición actual del iterador
+        *itr = original_value * 2;
+
+        // Mover el iterador a la siguiente posición
+        itr++;
+    }
+
+    // Mostrar los contenidos de la lista
+    for (int num : nums) {
+        cout << num << " ";  // Imprimir los elementos actualizados
+    }
+
+
+    
+
+
+
+    return 0;
+}
