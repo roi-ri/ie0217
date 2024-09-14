804472288e81618e26ee3e955a83cb22bc6dd39d
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-14 00:10:13 -0600

    Se agrega declaracion e implementacion de nodo a utilizar en el arbol binario
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

---
+++ Tareas/Tarea3/src/Node.cpp
@@ -0,0 +1,7 @@
+#include "Node.hpp"
+
+Node::Node(int valor){
+    data = valor;
+    left = nullptr;
+    right = nullptr; 
+};
---
+++ Tareas/Tarea3/src/Node.hpp
@@ -0,0 +1,15 @@
+#ifndef NODO_HPP
+#define NODO_HPP
+
+class Node {
+    public:
+        int data; 
+        Node* left;
+        Node* right;
+
+        Node(int valor);
+};
+
+
+
+#endif //NODO_HPP
