7a4bf6104bcc349cfef1db6336d403a89706e526
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-09-19 21:53:19 -0600

    Se agrega codigo inicial sobre excepciones
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

---
+++ Sesiones/Sesion8/Excepciones.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+
+    double numerator, denominator, divide;
+
+    cout << "Ingrese un numerador: ";
+    cin >> numerator; 
+
+    cout << "Enter denominator: ";
+    cin >> denominator; 
+
+
+    try {
+        //Tira una excepcion si el denominador es 0
+        if (denominator == 0) throw 0; 
+
+        //No ejecutado si el denominador es 0
+        divide = numerator / denominator;
+        cout << numerator << "/" << denominator << " = " << divide << endl; 
+    }
+
+    //Mensaje que se emeite en caso de que pase el caso del if 
+    catch (int num_exeption){
+        cout << "Error: No se puede dividir por " << num_exeption  << endl;
+          }
+
+    return 0; 
+
+}
