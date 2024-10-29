348a55de827dd1c998cffc37f14af7e10d6c7e94
roi-ri <rodrigo.e.sanchez.araya@gmail.com>
2024-10-28 23:20:43 -0600

    Se agrega codigo utilizado para pruebas Asan y Tsan
    Signed-off-by: roi-ri <rodrigo.e.sanchez.araya@gmail.com>

---
+++ Sesiones/Sesion15/PruebaAsan.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+#include<thread>
+#include<mutex> 
+
+int counter = 0; 
+std::mutex mtx; 
+
+void increment(){
+    for (int i = 0; i < 100000; ++i){
+        std::lock_guard<std::mutex> lock(mtx);
+        ++counter; // Condiccion de carrera
+    }
+}
+
+
+int main(){
+    std::thread t1(increment);
+    std::thread t2(increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Counter: " << counter << std::endl; 
+
+    return 0; 
+}
