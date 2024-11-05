6f7b72c54e0250cbff14089a7a7118f29c5a24ca
Rodri Sanchez <rodrigo@Macboock-129.local>
2024-11-05 17:28:50 -0600

    inicial commit para agregar archivos de este laboratorio
    Signed-off-by: Rodri Sanchez <rodrigo@Macboock-129.local>

---
+++ Laboratorios/Laboratorio10/CalculatorProject/src/Calculator.hpp
@@ -0,0 +1,14 @@
+// Calculator.hpp
+
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+class Calculator {
+public:
+    // Métodos para operaciones básicas
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+};
+#endif // CALCULATOR_HPP
---
+++ Laboratorios/Laboratorio10/CalculatorProject/tests/test_calculator.cpp
@@ -0,0 +1,27 @@
+// test_calculator.cpp
+
+#include <gtest/gtest.h>
+#include "../src/Calculator.hpp"
+
+Calculator calc;
+
+TEST(CalculatorTests, Addition) {
+    EXPECT_DOUBLE_EQ(calc.add(3.0, 4.0), 7.0);
+}
+
+TEST(CalculatorTests, Subtraction) {
+    EXPECT_DOUBLE_EQ(calc.subtract(10.0, 4.0), 6.0);
+}
+
+TEST(CalculatorTests, Multiplication) {
+    EXPECT_DOUBLE_EQ(calc.multiply(3.0, 5.0), 15.0);
+}
+
+TEST(CalculatorTests, Division) {
+    EXPECT_DOUBLE_EQ(calc.divide(10.0, 2.0), 5.0);
+}
+
+TEST(CalculatorTests, DivisionByZero) {
+    EXPECT_THROW(calc.divide(10.0, 0), std::invalid_argument);
+}
+
